4aa3f951e8d433139a131ea0d8a196e7038bebb1
SlaytherZR7 <slayther.zamora@epn.edu.ec>
2025-03-16 20:51:59 -0500

    add dark theme support using MUI in index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { createTheme, CssBaseline, ThemeProvider } from '@mui/material';
 
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
+
 const rootEl = document.getElementById('root');
 if (rootEl) {
   const root = ReactDOM.createRoot(rootEl);
   root.render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>,
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </ThemeProvider>,
   );
 }
