c0e50693f4e002006e3adb6e72fc618c1bdf1221
SlaytherZR7 <slayther.zamora@epn.edu.ec>
2025-03-16 21:10:42 -0500

    refactor: remove React.StrictMode wrapper from index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { createTheme, CssBaseline, ThemeProvider } from '@mui/material';
@@ -21,9 +20,7 @@ if (rootEl) {
   root.render(
     <ThemeProvider theme={darkTheme}>
       <CssBaseline />
-      <React.StrictMode>
-        <App />
-      </React.StrictMode>
+      <App />
     </ThemeProvider>,
   );
 }
