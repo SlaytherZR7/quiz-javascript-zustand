d70329245627c15c9847ea9fa0817a33f670bc74
SlaytherZR7 <slayther.zamora@epn.edu.ec>
2025-03-16 21:09:51 -0500

    add global styles and dark theme support in index.css

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { createTheme, CssBaseline, ThemeProvider } from '@mui/material';
+import './index.css';
 
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
